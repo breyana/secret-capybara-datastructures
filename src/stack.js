788c70a3f86238286e951abc1ead38319c081ab5
Breyana Scales <breyana@Breyanas-MacBook-Pro.local>
2017-02-13 17:13:27 -0800

    Stack - finished implementation and tests

--- src/stack.js
+++ src/stack.js
@@ -1,5 +1,49 @@
 'use strict'
 
+class Node {
+  constructor(data, next) {
+    this.data = data
+    this.next = next
+  }
+}
+
 export default class Stack {
-  // your code here
+  constructor() {
+    this.top = 0
+    this.size = 0
+  }
+
+  push(input) {
+    this.size++
+    this.top = new Node(input, this.top)
+  }
+
+  pop() {
+    if(this.size === 0) {
+      return null
+    }
+    this.size--
+    let poppedItem = this.top.data
+    this.top = this.top.next
+    return poppedItem
+  }
+
+  peek() {
+    if (this.size === 0) {
+      return null
+    }
+    return this.top.data
+  }
+
+  isEmpty() {
+    if (this.size === 0) {
+      return true
+    }
+    return false
+  }
+
+  length() {
+    return this.size
+  }
+
 }
